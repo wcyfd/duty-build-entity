305791d940f8f1551d087b90be53f86a87dfa3f0
aimfd <1101697681@qq.com>
2016-10-17 23:19:18 +0800

    add MagicConfig

---
+++ src/main/java/com/aim/duty/duty_build_entity/fo/MagicConfig.java
@@ -0,0 +1,41 @@
+package com.aim.duty.duty_build_entity.fo;
+
+public class MagicConfig {
+	private int magicId;
+	private String name;
+	private int maxValue;
+	private int maxDuration;
+
+	public int getMagicId() {
+		return magicId;
+	}
+
+	public void setMagicId(int magicId) {
+		this.magicId = magicId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getMaxValue() {
+		return maxValue;
+	}
+
+	public void setMaxValue(int maxValue) {
+		this.maxValue = maxValue;
+	}
+
+	public int getMaxDuration() {
+		return maxDuration;
+	}
+
+	public void setMaxDuration(int maxDuration) {
+		this.maxDuration = maxDuration;
+	}
+
+}
